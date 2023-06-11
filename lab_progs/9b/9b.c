9c0b223f05382289ebc17681650c7c50aca25979
Mohammad Mashhood Alam <mashhood2002@gmail.com>
2023-06-11 22:27:54 +0530

    refactor 9b.c

--- lab_progs/9b/9b.c
+++ lab_progs/9b/9b.c
@@ -1,25 +1,24 @@
 #include<stdio.h>
-int main()
-{
-int ch,rv;
-char cmd[10];
-rv=fork();
-if(rv==0)
-{
-do
-{
-printf("\nEnter a command\n");
-scanf("%s",cmd);
-system(cmd);
-printf("\n1 : continue\n0 : exit\n");
-scanf("%d",&ch);
-}
-while(ch!=0);
-}
-else
-{
-wait(0);
-printf("\nChild terminated\n");
-}
-return 0;
+int main() {
+    
+    int ch,rv; 
+    char cmd[10]; //string to store command
+    rv=fork(); // create child process
+
+    if(rv==0) { // if its child process
+        do {
+            // runs commands
+            printf("\nEnter a command\n");
+            scanf("%s",cmd);
+            system(cmd);
+            printf("\n1 : continue\n0 : exit\n");
+            scanf("%d",&ch);
+        }
+        while(ch!=0);
+    }
+    else { 
+        wait(0);
+        printf("\nChild terminated\n");
+    }
+    return 0;
 }
